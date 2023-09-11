f2580082fdf73a63449bc443928745fd608d7e04
hfzafrzl <144640775+hfzafrzl@users.noreply.github.com>
2023-09-11 09:30:25 +0700

    Create Luas_Segitiga.cpp

---
+++ Luas_Segitiga.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, t, L;
+	cout<<"menghitung luas segitiga \n";
+	cout<<"masukan alas: ";
+	cin>>a;
+	cout << "masukan tinggi: ";
+	cin>>t;
+	L=0.5*a*t;
+	cout<< " hasil luas segitiga adalah: "<<L<<endl;
+}
